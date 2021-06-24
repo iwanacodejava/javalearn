be8bacc02c67a17d4ac094a30ff2c4249f40b437
Alex Shishkin <gitbox@inbox.ru>
2021-06-24 19:31:33 +0300

    1

--- src/com/HellowWorld.java
+++ src/com/HellowWorld.java
@@ -2,6 +2,6 @@ package com;
 
 public class HellowWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("It's alive! It's alive!");
     }
 }
