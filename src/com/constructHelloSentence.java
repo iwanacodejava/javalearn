3aadb5678c6d5efadc7891efdce95d6366335645
Alex Shishkin <gitbox@inbox.ru>
2021-06-30 21:43:05 +0300

    3

---
+++ src/com/constructHelloSentence.java
@@ -0,0 +1,9 @@
+package com;
+
+public class constructHelloSentence {
+    public static void main (String[] args) {
+        String name = "hello";
+        String resultSentence = "Hello world! My name is " + name;
+        System.out.println(resultSentence);
+    }
+}
