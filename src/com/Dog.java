e36c84c4dc6db3abf76a2611a9fd6d7ee5e67024
Alex Shishkin <gitbox@inbox.ru>
2021-07-14 23:34:48 +0300

    dog

---
+++ src/com/Dog.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Dog {
+    int size;
+    String breed;
+    String name;
+    void bark () {
+        System.out.println("Гав! Гав!");
+    }
+}
---
+++ src/com/DogTestDrive.java
@@ -0,0 +1,9 @@
+package com;
+
+public class DogTestDrive {
+    public static void main (String [] args) {
+        Dog d = new Dog ();
+        d.size = 40;
+        d.bark();
+    }
+}
